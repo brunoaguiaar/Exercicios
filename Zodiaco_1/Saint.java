5764ff8344f5b0434ef94e0e02b488a9e4088833
BrunoAguiar <bruno.aguiar@cwi.com.br>
2017-04-13 17:43:34 -0300

    fix(Genero): comunicar classe genero com saint

--- Zodiaco_1/Saint.java
+++ Zodiaco_1/Saint.java
@@ -2,6 +2,7 @@ public class Saint {
     private String nome;
     private Armadura armadura;
     private boolean armaduraVestida;
+	private Genero genero = Genero.NAO_INFORMADO;
     
     public Saint(String nome, Armadura armadura) {
         this.nome = nome;
@@ -9,6 +10,19 @@ public class Saint {
     }
     
     public void vestirArmadura(){
-            this.armaduraVestida = true;
+    	this.armaduraVestida = true;
     }
+	
+	// camelCase
+	public boolean getArmaduraVestida(){
+		return this.armaduraVestida; 
+    }
+
+	public Genero getGenero(){
+		return genero;
+    }
+	
+	public void setGenero(Genero genero){
+		this.genero = genero;
+	}
 }
