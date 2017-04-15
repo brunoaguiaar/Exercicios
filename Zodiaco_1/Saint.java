1c0aa481f3c72fb26879d3ca2941cb74beed8deb
BrunoAguiar <bruno.aguiar@cwi.com.br>
2017-04-14 21:24:53 -0300

    feat(Saint): counicar classe status com o Saint

--- Zodiaco_1/Saint.java
+++ Zodiaco_1/Saint.java
@@ -2,7 +2,8 @@ public class Saint {
     private String nome;
     private Armadura armadura;
     private boolean armaduraVestida;
-	private Genero genero = Genero.NAO_INFORMADO;
+    private Genero genero = Genero.NAO_INFORMADO;
+    private Status status = Status.VIVO;
     
     public Saint(String nome, Armadura armadura) {
         this.nome = nome;
@@ -10,19 +11,28 @@ public class Saint {
     }
     
     public void vestirArmadura(){
-    	this.armaduraVestida = true;
+        this.armaduraVestida = true;
     }
-	
-	// camelCase
-	public boolean getArmaduraVestida(){
-		return this.armaduraVestida; 
+    
+    // camelCase
+    public boolean getArmaduraVestida(){
+        return this.armaduraVestida; 
     }
 
-	public Genero getGenero(){
-		return genero;
+    public Genero getGenero(){
+        return genero;
     }
-	
-	public void setGenero(Genero genero){
-		this.genero = genero;
-	}
+    
+    public void setGenero(Genero genero){
+        this.genero = genero;
+    }
+    
+    public Status getStatus(){
+        return status;
+    }
+    
+    public void getPerderVida(){
+        
+    }
+    
 }
