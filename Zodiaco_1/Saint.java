184059d6f69e64d307197bef7af438fedc743234
BrunoAguiar <bruno.aguiar@cwi.com.br>
2017-04-17 16:36:43 -0300

    feat(saint): implementar as constelações ao saint de ouro

--- Zodiaco_1/Saint.java
+++ Zodiaco_1/Saint.java
@@ -21,7 +21,18 @@ public class Saint {
         } else if (this.armadura.getCatArmadura() == Categoria.OURO){
             this.sentidosDespertados = 7;
             String constelacao = armadura.getConstelacao();
-            if (!constelacao.equals("Leao") && !constelacao.equals("Touro")){
+            if (!constelacao.equals("Leão") 
+            && !constelacao.equals("Touro")
+            && !constelacao.equals("Áries")
+            && !constelacao.equals("Gêmeos")
+            && !constelacao.equals("Câncer")
+            && !constelacao.equals("Virgem")
+            && !constelacao.equals("Libra")
+            && !constelacao.equals("Escorpião")
+            && !constelacao.equals("Sagitário")
+            && !constelacao.equals("Capricórneo")
+            && !constelacao.equals("Aquário")
+            && !constelacao.equals("Peixes")){
                 //dar erro
                 throw new Exception("Constelação inválida");
             }
