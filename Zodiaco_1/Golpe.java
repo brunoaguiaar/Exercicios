0b12aea09074522b3c3bf765d152443d4bb5c0b4
BrunoAguiar <bruno.aguiar@cwi.com.br>
2017-04-18 10:06:43 -0300

    feat(golpe): implementando a classe golpe.

---
+++ Zodiaco_1/Golpe.java
@@ -0,0 +1,14 @@
+public class Golpe{
+    private String nome;
+    private int dano;
+
+    public Golpe(String nome){
+        this.nome = nome;
+    }
+
+    public int fatorDano(int dano) {
+        return this.dano = dano;
+    }
+
+    
+}
