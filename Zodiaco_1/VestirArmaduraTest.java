e73234cd4f0e18430d0c21b70b0b58525ce8f4d7
BrunoAguiar <bruno.aguiar@cwi.com.br>
2017-04-24 15:51:01 -0300

    fix(VestirArmaduraTest): ajustar VestirArmaduraTest adicionando teste.

--- Zodiaco_1/VestirArmaduraTest.java
+++ Zodiaco_1/VestirArmaduraTest.java
@@ -1,42 +1,14 @@
-
-
 import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-/**
- * The test class VestirArmaduraTest.
- *
- * @author  (your name)
- * @version (a version number or a date)
- */
-public class VestirArmaduraTest
-{
-    /**
-     * Default constructor for test class VestirArmaduraTest
-     */
-    public VestirArmaduraTest()
-    {
-    }
-
-    /**
-     * Sets up the test fixture.
-     *
-     * Called before every test case method.
-     */
-    @Before
-    public void setUp()
-    {
-    }
-
-    /**
-     * Tears down the test fixture.
-     *
-     * Called after every test case method.
-     */
-    @After
-    public void tearDown()
-    {
+public class VestirArmaduraTest{
+    @Test
+    public void vestirArmadura() throws Exception{
+        GoldSaint bruno = new GoldSaint("Bruno", "Touro");
+        Movimento movimento = new VestirArmadura(bruno);
+        movimento.executar();
+        assertTrue(bruno.getArmaduraVestida());
     }
 }
