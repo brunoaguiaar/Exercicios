47b58005207249efc01652f0c56df314802ba742
BrunoAguiar <bruno.aguiar@cwi.com.br>
2017-05-11 17:15:58 -0300

    feat(JS): implementar primeiro exemplo de JS.

---
+++ Modulo_4/exemplos01.js
@@ -0,0 +1,18 @@
+console.log("EAE man kkk!!!");
+/*
+alert("SE É O BICHAO MEMO NE");
+pi = 3.14;
+*/
+if (typeof pi !== "undefined") {
+  console.log(pi);
+}
+
+//var somar = (a,b) => a+b;
+
+var somar = function(a, b){
+  return a+b;
+}
+
+function somaSemRetorno(a,b){
+  return a+b;
+}
