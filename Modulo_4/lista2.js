f1ddb91b4f7f3fdeee4d2474dc5e09c951ddf476
BrunoAguiar <bruno.aguiar@cwi.com.br>
2017-05-15 10:34:48 -0300

    feat(lista_02): implementar html e js da lista de exercicios 2, até exercicio 6 pronto.

---
+++ Modulo_4/lista2.js
@@ -0,0 +1,97 @@
+// exercicio 1
+function seriesInvalidas(series){
+  invalidas = new Array;
+  var date = new Date();
+  for (let i=0; i<series.legth; i++){
+    if(series[i].anoEstreia > data.getFullYear() ||
+    Object.values(series[1]) === "undefined" ||
+    Object.values(series[1]) === "null"){
+      invalidas.push(series[i].titulo);
+    }
+  }
+  return invalidas.toString();
+}
+
+
+//exercicio 2
+function filtrarSeriesPorAno(series, ano){
+  var seriesRetornadas = new Array;
+  for (let i=0; i<series.length; i++){
+    if(series[i].anoEstreia >= ano){
+      seriesRetornadas.push(series[i].titulo);
+    }
+  }
+  return seriesRetornadas;
+}
+
+
+// exercicio 3
+function mediaDeEpisodios(series){
+  var media = 0;
+  for (let i=0; i<series.length; i++){
+    media = media + series[i].numeroEpisodios;
+  }
+  media = media/series.length;
+  return media;
+}
+
+
+// exercicio 4
+function procurarPorNome(series, nome){
+  var nomeRetornado = false;
+  for (let i=0; i<series.length; i++){
+    for (let j=0; j<series[i].elenco.length; j++){
+      if(series[i].elenco[j].match(nome)){
+        nomeRetornado = true;
+      }
+    }
+  }
+  return nomeRetornado
+}
+
+
+// exercicio 5
+function mascadaEmSerie(series){
+  var diretor = 100000 * series.diretor.length;
+  var elenco = 40000 * series.elenco.length;
+  var total = diretor + elenco;
+  return total;
+}
+
+
+// exercicio 6
+function queroGenero(genero){
+  seriesGenero = new Array;
+  for (let i=0; i<series.length; i++){
+    for (let j=0; j<series[i].genero.length; j++){
+      if(series[i].genero[j].match(genero)){
+        seriesGenero.push(series[i].titulo)
+      }
+    }
+  }
+  return seriesGenero;
+}
+
+function queroTitulo(titulo){
+  seriesTitulo = new Array;
+  for (let i=0; i<series.length; i++){
+    if(series[i].titulo.match(titulo)){
+      seriesTitulo.push(series[i].titulo)
+    }
+  }
+  return seriesTitulo;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
