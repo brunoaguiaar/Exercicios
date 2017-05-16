41924c492cfdc2ddfd84ea3dc3d6ba9030a06727
BrunoAguiar <bruno.aguiar@cwi.com.br>
2017-05-16 17:30:45 -0300

    feat(exercicio): implementar começo do exercicio de javascript com botao no html.

--- Modulo_4/pokeapi.js
+++ Modulo_4/pokeapi.js
@@ -1,17 +1,27 @@
-let urls = [
-  "http://pokeapi.co/api/v2/pokemon/99/",
-  "http://pokeapi.co/api/v2/pokemon/6/",
-  "http://pokeapi.co/api/v2/pokemon/7/"
-]
-urls.forEach(url => {
-  fetch(url)
-    .then(response => response.json())
-    .then(json => {
-      console.log(json);
-      console.log(json.sprites.front_default);
-      let div = document.getElementById('detalhesPokemon');
-      let img = document.createElement('img');
-      img.src = json.sprites.front_default;
-      div.append(img);
-    })
+// let urls = [
+//   "http://pokeapi.co/api/v2/pokemon/99/",
+//   "http://pokeapi.co/api/v2/pokemon/6/",
+//   "http://pokeapi.co/api/v2/pokemon/7/"
+// ]
+// urls.forEach(url => {
+//   fetch(url)
+//     .then(response => response.json())
+//     .then(json => {
+//       console.log(json);
+//       console.log(json.sprites.front_default);
+//       let div = document.getElementById('detalhesPokemon');
+//       let img = document.createElement('img');
+//       img.src = json.sprites.front_default;
+//       div.append(img);
+//     })
+// })
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  let btnPesquisar = document.getElementById('btnPesquisar');
+  btnPesquisar.onclick = function() {
+    console.log("clicou...");
+  }
 })
