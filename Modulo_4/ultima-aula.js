bfa35771fc99ad7cdc67e551beed16f65ef058ad
BrunoAguiar <bruno.aguiar@cwi.com.br>
2017-05-16 16:22:07 -0300

    feat(Jedi): implementar exemplo de Jedi feito em aula.

--- Modulo_4/ultima-aula.js
+++ Modulo_4/ultima-aula.js
@@ -4,3 +4,18 @@ Number.prototype.arredondar = function(casasDecimais = 2) {
   let casas = Math.pow(10, casasDecimais);
   return Math.round(this * casas) / casas;
 }
+
+
+function Jedi() {
+  this.nome = "Luke";
+  this.ligarSabre = function() {
+    //let self = this;
+    //setInterval(function(){
+    //console.log("Uóóóóóm", this.nome)
+    //}.bind(this), 2000)
+    setInterval(() =>{
+      console.log("Uóóóóóm", this.nome);
+    }, 2000)
+  }
+  console.log(this);
+}
