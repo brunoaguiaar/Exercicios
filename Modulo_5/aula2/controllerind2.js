12119875a058d70a48c42b8c4f30e1545c266345
BrunoAguiar <bruno.aguiar@cwi.com.br>
2017-05-18 17:09:15 -0300

    feat(exercicio2): implementar exercicio 2 resolvido da segunda aula de angular.

---
+++ Modulo_5/aula2/controllerind2.js
@@ -0,0 +1,44 @@
+var modulo = angular.module('aula02-2', []);
+
+modulo.filter('mascada', function(){
+  return function(nome){
+    return nome.replace(/nunes/i, '$ Nunes $');
+  }
+})
+
+modulo.controller('Exemplo2', function($scope) {
+  $scope.instrutores = [{
+    nome: 'Bernardo',
+    aula: [{
+        numero: 1,
+        nome: 'OO'
+      },
+      {
+        numero: 4,
+        nome: 'Javascript'
+      }
+    ]
+  },
+  {
+    nome: 'Nunes',
+    aula: [{
+      numero: 2,
+      nome: 'Banco de Dados I'
+    }]
+  },
+  {
+    nome: 'Pedro (PHP)',
+    aula: [{
+      numero: 3,
+      nome: 'HTML e CSS'
+    }]
+  },
+  {
+    nome: 'Zanatta',
+    aula: [{
+      numero: 5,
+      nome: 'AngularJS'
+    }]
+  }
+];
+});
