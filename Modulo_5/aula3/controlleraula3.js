53314791701840d94e8b37f5730fc9a851deb110
BrunoAguiar <bruno.aguiar@cwi.com.br>
2017-05-19 17:37:49 -0300

    feat(formulario): implementar exercicio de  formulario

--- Modulo_5/aula3/controlleraula3.js
+++ Modulo_5/aula3/controlleraula3.js
@@ -4,10 +4,32 @@ modulo.controller('ExemploController', function($scope) {
 
   $scope.nomes = ['Bernardo', 'Nunes'];
 
+});
 
-  $scope.incluir = function(novoNome){
-    if($scope.meuForm.$invalid) return;
-    $scope.nomes.push(novoNome);
+modulo.controller('Exemplo2', function($scope,$filter) {
+
+  $scope.instrutores = [{
+    nome: 'Bernardo',
+    sobrenome: 'Rezende',
+    idade: 30,
+    email: 'bernardo@cwi.com.br',
+    jaDeuAula: true,
+    aula: 'OO'
+  }]
+
+  $scope.aulas = [
+    'OO',
+    'HTML e CSS',
+    'Javascript',
+    'AngularJS',
+    'Banco de Dados I'
+  ];
+
+  $scope.incluir = function(){
+    if($scope.meuForm.$invalid){
+      let copiaDoNovoInstrutor = angular.copy($scope.novoInstrutor);
+      $scope.instrutores.push(copiaDoNovoInstrutor);
+      $scope.novoInstrutor = {};
+    }
   }
-
 });
