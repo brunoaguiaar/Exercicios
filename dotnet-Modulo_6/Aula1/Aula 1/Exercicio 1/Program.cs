2aab11d2ea0489ffb0de83e4d7fc72d5061953bf
BrunoAguiar <bruno.aguiar@cwi.com.br>
2017-05-24 17:31:56 -0300

    feat(.NET): implementar primeiro dia de aula de .NET.

---
+++ dotnet-Modulo_6/Aula1/Aula 1/Exercicio 1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> entradas = new List<int>();
+
+            while (true)
+            {
+                Console.WriteLine("Digite um valor");
+                var entrada = Console.ReadLine();
+
+                if (entrada == "exit")
+                {
+                    break;
+                }
+
+                entradas.Add(int.Parse(entrada));
+            }
+
+            foreach (var entrada in entradas)
+            {
+                Console.WriteLine(entrada);
+            }
+            Console.ReadKey();
+        }
+    }
+}
