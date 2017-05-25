8407edbeb7485713546298a66f66d2e009eb3d26
BrunoAguiar <bruno.aguiar@cwi.com.br>
2017-05-25 16:54:10 -0300

    feat(aula2): implementar aula 2 de c#.

---
+++ dotnet-Modulo_6/Aula2/Arquivo/Arquivo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arquivo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string arquivo = @"C:\Users\bruno.aguiar\Desktop\teste.txt";
+
+            List <String> linhas = new List<String>();
+
+            using (var reader = new StreamReader(arquivo))
+           {
+                var linha = reader.ReadLine();
+
+                Console.WriteLine(linhas);
+                
+                Console.ReadKey();
+           }
+        }
+    }
+}
