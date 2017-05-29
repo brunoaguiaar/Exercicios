6eb323a5ca46827cdd5809bdcee1c8ee09a07e3c
BrunoAguiar <bruno.aguiar@cwi.com.br>
2017-05-29 17:28:58 -0300

    feat(aula4): implementar aula 4 de c# e começo do exercicio.

---
+++ dotnet-Modulo_6/Aula4/Demo1/Demo1.Infraestrutura/Repositorios/PedidoRepositorio.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo1.Dominio.Entidades;
+
+namespace Demo1.Infraestrutura.Repositorios
+{
+    class PedidoRepositorio : IPedidoRepositorio
+    {
+        public void Alterar(Pedido pedido)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Criar(Pedido pedido)
+        {
+            //realizar o insert do pedido
+            //obter o ultimo id do pedido (SELECT @@IDENTYTI)
+
+            //para cada item do pedido, realizar o insert do ItemPedido
+            //obter o ultimo id do ItemPedido (SELECT @@IDENTYTI)
+        }
+
+        public void Excluir(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Pedido> Listar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Pedido Obter(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ dotnet-Modulo_6/Aula4/Demo1/Demo1.Infraestrutura/Repositorios/ProdutoRepositorio.cs
@@ -0,0 +1,80 @@
+﻿using Demo1.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1.Infraestrutura.Repositorios
+{
+
+    public class ProdutoRepositorio
+    {
+        string stringConexao = "Server=13.65.101.67; User Id=bruno.aguiar;Password=123456;Database=aluno15db";
+
+        public void Criar(Produto produto)
+        {
+
+            using (var conexao = new SqlConnection(stringConexao))
+            {
+                conexao.Open();
+
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandText = @"insert into produto(Nome, Preco, Estoque) values(@nome, @preco, @estoque)";
+
+                    comando.Parameters.AddWithValue("@nome", produto.Nome);
+                    comando.Parameters.AddWithValue("@preco", produto.Preco);
+                    comando.Parameters.AddWithValue("@estoque", produto.Estoque);
+
+                    // executa o comando e retorna somente a quantidade de linhas afetadas
+                    comando.ExecuteNonQuery();
+
+                    // lê o resultado de um SELECT
+                    //comando.ExecuteReader();
+
+                    // executa o comando e retorna o primeiro resultado
+                    //comando.ExecuteScalar();
+                }
+
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandText = "SELECT @@IDENTITY";
+
+                    var result = (decimal)comando.ExecuteScalar();
+                    produto.Id = (int)result;
+                }
+            }
+        }
+
+        public List<Produto> Listar()
+        {
+            var produtos = new List<Produto>();
+
+            using (var conexao = new SqlConnection(stringConexao))
+            {
+                conexao.Open();
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandText = "SELECT Id, Nome, Preco, Estoque FROM Produto";
+
+                    var dataReader = comando.ExecuteReader();
+                    while (dataReader.Read())
+                    {
+                        var produto = new Produto();
+
+                        produto.Id = (int)dataReader["Id"];
+                        produto.Nome = (string)dataReader["Nome"];
+                        produto.Preco = (decimal)dataReader["Preco"];
+                        produto.Estoque = (int)dataReader["Estoque"];
+
+                        produtos.Add(produto);
+                    }
+                }
+            }
+
+            return produtos;
+        }
+    }
+}
