e25056aaca2bad45ccbd034a20ab042af2a8e14a
BrunoAguiar <bruno.aguiar@cwi.com.br>
2017-06-05 21:08:12 -0300

    feat(SpeedLocadora): implementar classes Locação e Pacote ao projeto de C#.

---
+++ dotnet-Modulo_6/Trabalho/SpeedLocadora/SpeedLocadora.Infraestrutura/Entidade/Locacao.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeedLocadora.Infraestrutura.Entidade
+{
+    class Locacao
+    {
+        public int IdLocacao { get; set; }
+        public DateTime DataLocacao { get; set; }
+        public DateTime DataEntrega { get; set; }
+        public DateTime PrevisaoEntrega { get; set; }
+        public double PrecoFinal { get; set; }
+        public Pacote IdPacote { get; set; }
+        public Acessorio IdAcessorio { get; set; }
+        public Jogo IdJogo { get; set; }
+        public Console IdConsole { get; set; }
+    }
+}
---
+++ dotnet-Modulo_6/Trabalho/SpeedLocadora/SpeedLocadora.Infraestrutura/Entidade/Pacote.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeedLocadora.Infraestrutura.Entidade
+{
+    class Pacote
+    {
+        public int IdPacote { get; set; }
+        public Jogo IdJogo { get; set; }
+        public string Tipo { get; set; }
+        public double Valor { get; set; }
+        public Console IdConsole { get; set; }
+        public Acessorio IdAcessorio { get; set; }
+    }
+}
