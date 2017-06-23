76cd985786fc0f4941eebbadedb20af584b72c82
BrunoAguiar <bruno.aguiar@cwi.com.br>
2017-06-23 17:21:19 -0300

    feat(aula4): implementar aulguns exemplos de JPA da aula 4 de java.

---
+++ Modulo_8/Aula3/src/main/java/br/com/crescer/aula3/SQLUtils.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.crescer.aula3;
+
+import java.io.File;
+
+/**
+ *
+ * @author bruno.aguiar
+ */
+public interface SQLUtils {
+
+    void runFile(String filename);
+
+    String executeQuery(String query);
+    
+    void importCSV(File file);
+    
+    File exportCSV(String query);
+
+}
